f8690ee3da7fdea4121b96e003e817c0d6c8624f
Mikhail Glushkov <miggl@mail.ru>
2023-10-17 11:47:00 +0300

    return function by default and fix test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,9 +9,12 @@ def array_fixture():
 
 def test_get(array_fixture):
     assert arrs.get(array_fixture, 1, "test") == 2
-    assert arrs.get([], 0, "test") == None
+    assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice(array_fixture):
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice(array_fixture, 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice(array_fixture) == [1, 2, 3]
+
